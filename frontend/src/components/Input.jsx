27a838d963543e387b956896957df184d21bf9d1
kathirKarthik001 <kathirkarthik001@gmail.com>
2024-02-08 15:12:02 +0530

    frontend template added

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,14 @@
+
+function Input({name,value,placeholder}) {
+  return (
+    <div>
+        <form>
+            <input type="text"
+            
+            />
+        </form>
+    </div>
+  )
+}
+
+export default Input
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../components/Logo'
+
+function login() {
+  return (
+    <>
+    <Logo width='260px'/>
+    </>
+  )
+}
+
+export default login
