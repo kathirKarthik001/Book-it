456da68be9b56beb486c757cbef359ccf1bcbdce
Mouniesh <iammounieshv@gmail.com>
2024-09-12 22:20:28 +0530

    navbar responsiveness added in [header.jsx]

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,131 +1,103 @@
-import React from 'react'
-import Logo from './Logo'
-import {Link , useNavigate} from 'react-router-dom'
-import {FaSignInAlt , FaSignOutAlt , FaUser } from 'react-icons/fa'
-import { useDispatch , useSelector} from 'react-redux'
-import { logout, reset } from '../features/auth/authSlice'
-import { toast } from 'react-toastify'
+/* eslint-disable react/prop-types */
+import Logo from './Logo';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaSignInAlt, FaSignOutAlt, FaBars, FaTimes } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
+import { useState, useEffect } from 'react';
 
-function Header({onSelect}) {
-   const {user} = useSelector((state) => state.auth)
-   const dispatch = useDispatch()
-   const navigate = useNavigate()
+function Header({ onSelect }) {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
-   const onTouch = (val) =>{
-    onSelect(val)
-   }
+  const onTouch = (val) => {
+    onSelect(val);
+    setMenuOpen(false); // Close menu on selection
+  };
 
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/login');
+  };
 
+  const toggleMenu = () => {
+    setMenuOpen((prevMenuOpen) => {
+      const newState = !prevMenuOpen;
+      console.log("Toggling menu. Previous state:", prevMenuOpen, "New state:", newState);
+      return newState;
+    });
+  };
 
-   const handleLogout =() =>{
-    dispatch(logout())
-    dispatch(reset())
-    navigate('/login')
+  useEffect(() => {
+    console.log("Menu open state has changed:", menuOpen);
+  }, [menuOpen]);
+
+  if (!user || user.message) {
+    return (
+      <header className='header'>
+        <div className="logo">
+          <Link to='/'> <Logo width='70px' /> </Link>
+        </div>
+        <div>
+          <h1>Register</h1>
+        </div>
+        <Link to='/login'>
+          <div className='login-button'>
+            <FaSignInAlt />Login
+          </div>
+        </Link>
+      </header>
+    );
+  }
+
+  if (user) {
+    const { role } = user;
+    return (
+      <header className='header'>
+        <div className="logo">
+          <Link to='/'> <Logo width='70px' /> </Link>
+        </div>
+        <button className='menu-toggle' onClick={toggleMenu}>
+          {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+        <nav className={`nav-links ${menuOpen ? 'active' : ''}`}>
+          <ul className='nav-items'>
+            {role === 'admin' ? (
+              <>
+                <li>
+                  <button className='tab' onClick={() => onTouch(1)}> Home</button>
+                </li>
+                <li>
+                  <button className='tab' onClick={() => onTouch(2)}>Events</button>
+                </li>
+                <li>
+                  <button className='tab' onClick={() => onTouch(3)}>Halls</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <button className='tab' onClick={() => onTouch(4)}>Reserve</button>
+                </li>
+                <li>
+                  <button className='tab' onClick={() => onTouch(2)}>Events</button>
+                </li>
+                <li>
+                  <button className='tab' onClick={() => onTouch(5)}>Bookings</button>
+                </li>
+              </>
+            )}
+          </ul>
+          <button className='btn' onClick={handleLogout}>
+            <FaSignOutAlt /> Logout
+          </button>
+        </nav>
+      </header>
+    );
+  }
 }
 
-// Register page
-   if(!user || user.message){
-   
-        return (
-            <>
-            <header className='header'>
-                <div className="logo">
-                    <Link to='/' > <Logo width='70px' /> </Link>
-                </div>
-        
-            
-        
-                <div>
-                    <h1>Register</h1>
-                </div>
-            
-                <Link to='/login'>
-                    <div className='login-button'>
-                        <FaSignInAlt/>Login       
-                    </div>
-                </Link>
-                
-        
-            </header>
-            </>
-        )
-   }
-
-   if(user){
-        const {role} = user
-
-        if(role === 'admin'){
-            return (
-                <>
-                <header className='header'>
-                    <div className="logo">
-                        <Link to='/'> <Logo width='70px' /> </Link>
-                    </div>
-            
-                    <ul>
-                        <li>
-                            <button className='tab' onClick={()=>onTouch(1)}> Home</button>
-                        </li>
-                        <li>
-                            <button className='tab' onClick={()=>onTouch(2)}>Events</button>
-                        </li>
-                        <li>
-                            <button className='tab' onClick={()=>onTouch(3)}>Halls</button>
-                        </li>
-                    </ul>
-                
-                    
-                    <button className='btn' onClick={handleLogout}>
-                            <FaSignOutAlt/> Logout
-                   </button>
-                
-                    
-            
-            
-            
-                </header>
-                </>
-            )
-        }
-        else if(role === 'user'){
-            return (
-                <>
-                <header className='header'>
-                    <div className="logo">
-                        <Link to='/' > <Logo width='70px' /> </Link>
-                    </div>
-            
-                    <ul>
-                        <li>
-                            <button className='tab' onClick={()=>onTouch(4)}>Reserve</button>
-                        </li>
-                        <li>
-                            <button className='tab' onClick={()=>onTouch(2)}>Events</button>
-                        </li>
-                        <li>
-                            <button className='tab' onClick={()=>onTouch(5)}>Bookings</button>
-                        </li>
-                    </ul>
-                
-                     <button className='btn' onClick={handleLogout}>
-                           <FaSignOutAlt/> Logout
-                    </button>
-                    
-                    
-            
-            
-            
-                </header>
-                </>
-            )
-        }
-   }
-
-//    if(user.message){
-//     handleLogout()
-//     toast.error('login failed')
-//    }
-  
-}
-
-export default Header
+export default Header;
