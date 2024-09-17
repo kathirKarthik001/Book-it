e966650c0a4f2233907022074c367e8d8a52fbb4
Kathir Karthik  M <kathirkarthik001@gmail.com>
2024-09-17 15:59:48 +0530

    Update server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,7 @@
 const express =  require('express')
 const  dotenv = require('dotenv').config()
 const colors = require('colors')
+const cors = require('cors');
 
 const connectDB = require('./config/db')
 const ScheduledJob = require('./service/scheduledJob')
